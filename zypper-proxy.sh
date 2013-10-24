9a0697f03505eafb4cf96c629474b6f6c47f16ea
Gustavo Lichti <gustavo.lichti@gmail.com>
2013-10-24 02:44:51 -0200

    First commit

---
+++ zypper-proxy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ "$1" = 'add' ]; then
+	if [ -n "$2" ];then
+		for FILE in /etc/zypp/repos.d/*
+		do
+			if [ $(grep proxy $FILE | wc -l) -eq 0 ];then
+				echo "proxy=$$2" >> $FILE
+			else
+				sed -i s/proxy.*/proxy=$2/g $FILE
+			fi
+		done
+	else
+		echo "use: $0 add 0.0.0.0"
+	fi
+elif [ "$1" = 'del' ]; then
+	for FILE in /etc/zypp/repos.d/*
+	do
+		if [ $(grep proxy $FILE | wc -l) -gt 0 ];then
+			sed -i s/proxy.*//g $FILE
+		fi
+	done
+else
+	echo "use: $0 add 0.0.0.0"
+fi
